52c6319638be96ff5aa61c3a782df2fa5c4a17e6
Théotime COLIN <contact@theotimecolin.com>
2020-10-02 18:25:36 +0200

    junk(Bots): disable all but Quokka & Panda

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require("dotenv").config()
 const MongoClient = require('mongodb').MongoClient
 const mongoose = require('mongoose')
 const moment = require('moment')
-const Capybara = require('./bots/capybara')
-const Pangolin = require('./bots/pangolin')
-const Bard = require('./bots/bard')
-const Quokka = require('./bots/quokka')
 const Panda = require('./bots/panda')
+const Quokka = require('./bots/quokka')
+// const Capybara = require('./bots/capybara')
+// const Pangolin = require('./bots/pangolin')
+// const Bard = require('./bots/bard')
 //const Wendigo = require('./bots/wendigo')
 //const Ellieth = require('./bots/ellieth')
 //const Bridgette = require('./bots/bridgette')
@@ -21,9 +21,9 @@ mongoose.connect(process.env.MONGO, {
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', () => {
-    const capybaraBot = new Capybara()
-    const pangolinBot = new Pangolin()
-    const bardBot = new Bard()
+    // const capybaraBot = new Capybara()
+    // const pangolinBot = new Pangolin()
+    // const bardBot = new Bard()
     const quokkaBot = new Quokka()
     const pandaBot = new Panda()
     //const wendigoBot = new Wendigo()
